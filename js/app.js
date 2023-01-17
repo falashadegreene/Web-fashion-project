9b69975060c77d25743dbda3dda3a64a2992f3b7
Falashade Greene <falashadegreene@gmail.com>
2023-01-16 22:49:18 -0500

    added styling to header and centered items

--- js/app.js
+++ js/app.js
@@ -1,23 +1,3 @@
 'use script'
 
 
-// // Answer to guessing game
-// let myAge = 35; 
-
-// // setting condition by creating a for loop if answer is too low or high.
-// for( i = 0; i < 4; i++) {
-//   let userGuess = parseInt(prompt('Guess my age'));
-//   if(myAge === userGuess){
-//     alert('that is correct!')
-//     break;
-
-//   } else if(userGuess > myAge){
-//     alert('too high. Try again')
-//   } else if(userGuess < myAge){
-//     alert('too low. Try again')
-//   }
- 
-//   if(i == 3){
-//     alert('Sorry! you maxed out your guesses');
-//   }  
-// }
