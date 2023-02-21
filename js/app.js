cdefb887745d1cc5ca1a1077fd7975049661f8b8
Falashade Greene <falashadegreene@gmail.com>
2023-02-20 23:52:25 -0500

    added close function for modal

--- js/app.js
+++ js/app.js
@@ -1,44 +1,84 @@
-'use strict';
+"use strict";
 
 // Event listner for image 
 
-const images = document.querySelectorAll('.img_item img');
+const images = document.querySelectorAll(".img_item img");
+let imgIndex
 let imgAtt;
 
 // forEach loop for event listner to execute click on each image
 
-images.forEach((img) => {
-  img.addEventListener('click', (e) => {
+images.forEach((img, i) => {
+  img.addEventListener("click", (e) => {
     imgAtt = e.target.src;
     console.log(imgAtt);
-    imgModal();
+    imgModal(imgAtt);
+    imgIndex = i;
   });
 });
 
-// Modal 
+// Creating Modal 
 
 let imgModal = (src) => {
-  const modal = document.createElement('div');
-  modal.setAttribute('class', 'modal');
-  document.querySelector('.img-flex').append(modal);
-  const nextImage = document.createElement('img');
-  nextImage.setAttribute('src', src);
-  modal.append(nextImage)
+  const modal = document.createElement("div");
+  modal.setAttribute("class", "modal");
+  document.querySelector(".img-flex").append(modal);
+
+// adding image to Modal 
+  const newImage = document.createElement("img");
+  newImage.setAttribute("src", src);
+  modal.append(newImage);
 };
-
-
 
 //closing image button 
 
 const closeImg = document.createElement("i");
-closeImg.setAttribute("class", "fas fa-times closeBtn");
+closeImg.setAttribute("class", "fas fa-times closeImg");
 
 closeImg.onclick = () => {
   modal.remove();
 };
-modal.append(nextImage, closeImg);
+modal.append(newImage, closeImg);
 
 // Next and previous arrows for images 
+
+const nextBtn = document.createElement("i");
+nextBtn.setAttribute("class", "fas fa-angle-right nextBtn");
+
+nextBtn.onClick = () => {
+  nextImage.setAttribute("src", "nextImg"())
+};
+
+const prevBtn = document.createElement("i");
+prevBtn.setAttribute("class", "fas fa-angle-left prevBtn");
+
+prevBtn.onClick = () => {
+  prevImage.setAttribute("src", "prevImg"())
+}
+modal.append(newImage, closeImg, nextBtn, prevBtn);
+
+
+// next image function 
+
+let nextImg = () => {
+  imgIndex++;
+
+  if(imgIndex >= images.length) {
+    imgIndex = 0
+  }
+  return images[imgIndex].src;
+};
+
+// prev image function 
+
+let prevImg = () => {
+  imgIndex--;
+
+  if(imgIndex < 0 ) {
+    imgIndex = images.length - 1
+  }
+  return images[imgIndex].src
+}
 
 
 
