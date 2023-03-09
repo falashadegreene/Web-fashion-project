62d9897eaa80daec399dd9f6074f74ffae8ae10f
Falashade Greene <falashadegreene@gmail.com>
2023-03-09 18:10:21 -0500

    added dark mode functionality in js file

--- js/home.js
+++ js/home.js
@@ -1,2 +1,9 @@
 "use strict"
 
+
+// credit W3schools https://www.w3schools.com/howto/tryit.asp?filename=tryhow_js_toggle_dark_mode
+
+function clickFunction() {
+  let element = document.body;
+  element.classList.toggle("dark");
+}
