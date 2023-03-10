b2c15ff4c31e5d57fbc9c16f5a237d498838f6f7
Falashade Greene <falashadegreene@gmail.com>
2023-03-09 20:59:27 -0500

    commented out next and prev button in css for modal

--- js/home.js
+++ js/home.js
@@ -1,9 +1,8 @@
 "use strict"
 
-
-// credit W3schools https://www.w3schools.com/howto/tryit.asp?filename=tryhow_js_toggle_dark_mode
-
 function clickFunction() {
   let element = document.body;
   element.classList.toggle("dark");
 }
+
+
